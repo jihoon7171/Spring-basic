d8761f7677fa32101cb7a28bf95720ea047b46f5
jihoon7171 <jihoon7171@naver.com>
2024-11-15 01:57:56 +0900

    Section -9

--- src/test/java/lifecycle/BeanLIfeCycleTest.java
+++ src/test/java/lifecycle/BeanLIfeCycleTest.java
@@ -1,2 +1,29 @@
-package lifecycle;public class BeanLIfeCycleTest {
+package lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLIfeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        Networkclient client = ac.getBean(Networkclient.class);
+
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+        //@Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
+        public Networkclient networkclient(){
+            Networkclient networkclient = new Networkclient();
+            networkclient.setUrl("www.asdf.com");
+            return networkclient;
+        }
+    }
+
 }
--- src/test/java/lifecycle/Networkclient.java
+++ src/test/java/lifecycle/Networkclient.java
@@ -1,8 +1,16 @@
-public class Networkclient {
+package lifecycle;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Networkclient  {
 
     private String url;
     public Networkclient(){
         System.out.println("생성자 호출 " );
+
     }
     public void setUrl(String url){
         this.url = url;
@@ -19,5 +27,13 @@ public class Networkclient {
         System.out.println("disconnect "+url);
     }
 
-
+    @PostConstruct
+    public void init() {
+        connect();
+        call("초기화 연결 메세지");
+    }
+    @PreDestroy
+    public void close() {
+        disconnect();
+    }
 }
